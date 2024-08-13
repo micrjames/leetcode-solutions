1d630907adaeff074bc06f24fe033038d06a53fb
Michael James <micrjames921@gmail.com>
2024-08-13 15:36:59 -0700

    completed tests for 'prodExcSelf.Cmp'

--- tests/prodExcSelf.test.ts
+++ tests/prodExcSelf.test.ts
@@ -3,7 +3,6 @@ import { prodExcSelf } from "../arrays&hashing/prodExcSelf";
 describe("An array of numbers.", () => {
    let inputs: Array<Array<number>>;
    let outputs: Array<Array<number>>;
-   let answers: Array<Array<number>>;
 
    beforeAll(() => {
 	  inputs = [
@@ -14,15 +13,26 @@ describe("An array of numbers.", () => {
 		 [24, 12, 8, 6],
 		 [0, 0, 9, 0, 0]
 	  ];
-
-	  answers = [
-		 prodExcSelf.Cmp(inputs[0]),
-	     prodExcSelf.Cmp(inputs[1])
-	  ];
    });
    describe("Compared", () => {
-	  test.todo("Should contain the correct number of elements.");
-	  test.todo("Should contain the correct elements.");
+	  let answers: Array<Array<number>>;
+	  beforeAll(() => {
+		 answers = [
+			prodExcSelf.Cmp(inputs[0]),
+			prodExcSelf.Cmp(inputs[1])
+		 ];
+	  });
+	  test("Should contain the correct number of elements.", () => {
+		 for(const [idx, answer] of answers.entries())
+			expect(answer).toHaveLength(outputs[idx].length);
+	  });
+	  test("Should contain the correct elements.", () => {
+		 for(const [idx, answer] of answers.entries())
+			for(const [numIdx, num] of answer.entries()) {
+			   const isAnswerOutput = num === outputs[idx][numIdx];
+			   expect(isAnswerOutput).toBeTruthy();
+			}
+	  });
    });
    describe("Compared in Place", () => {
 	  test.todo("Should contain the correct number of elements.");
