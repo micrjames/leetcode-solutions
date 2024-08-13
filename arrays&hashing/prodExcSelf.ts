6c8cc2b25c1370cbdd7294f573e0f6b8db014176
Michael James <micrjames921@gmail.com>
2024-08-13 14:23:25 -0700

    wrote code for 'Cmp'

--- arrays&hashing/prodExcSelf.ts
+++ arrays&hashing/prodExcSelf.ts
@@ -6,7 +6,21 @@
 // a. Compare 
 
 const Cmp = (nums: number[]): number[] => {
-   return Array<number>();
+   let answer: Array<number> = Array.from({length: nums.length}, _ => 0);
+   let prefix: Array<number> = Array.from({length: 1}, _ => 1);
+   let postfix: Array<number> = Array.from({length: 1}, _ => 1);
+
+   for(const num of nums)
+	  prefix = [...prefix, num * prefix[prefix.length-1]];
+
+   for(const num of nums.reverse())
+	  postfix = [...postfix, num * postfix[postfix.length-1]];
+   postfix.reverse();
+
+   for(let it = 0; it < answer.length; it++)
+	  answer[it] = prefix[it] * postfix[it+1];
+
+   return answer;
 };
 
 //	b. Compare in Place
--- tests/prodExcSelf.test.ts
+++ tests/prodExcSelf.test.ts
@@ -1,8 +1,9 @@
 import { prodExcSelf } from "../arrays&hashing/prodExcSelf";
 
 describe("An array of numbers.", () => {
-   let inputs: number[][];
-   let outputs: number[][];
+   let inputs: Array<Array<number>>;
+   let outputs: Array<Array<number>>;
+   let answers: Array<Array<number>>;
 
    beforeAll(() => {
 	  inputs = [
@@ -14,7 +15,12 @@ describe("An array of numbers.", () => {
 		 [0, 0, 9, 0, 0]
 	  ];
 
+	  answers = [
+		 prodExcSelf.Cmp(inputs[0]),
+	     prodExcSelf.Cmp(inputs[1])
+	  ];
+	  console.log(outputs[1], answers[0]);
+	  console.log(outputs[1], answers[1]);
    });
-   test.todo("Should contain as elements products of other elements.");
-   test.todo("Should contain as elements products of other elements, computed in place.");
+   test("Should contain ${inputs[0]} elements", () => {});
 });
